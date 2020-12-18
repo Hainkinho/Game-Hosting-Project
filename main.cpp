7fc7839e382a3d19b8a0408f5f4b030aec9319dd
Hainkinho <flohainka@gmail.com>
2020-12-18 11:48:36 +0100

    minor changes

--- main.cpp
+++ main.cpp
@@ -8,11 +8,6 @@ using namespace std;
 
 
 int main() {
-    // shared_ptr<Player> player1 = std::make_shared<Player>(Player("Leia", 100));
-    // shared_ptr<Player> player2 = std::make_shared<Player>(Player("Lara", 109));
-    // shared_ptr<Player> player3 = std::make_shared<Player>(Player("Oscar", 1000));
-    // shared_ptr<Player> player4 = std::make_shared<Player>(Player("Geschi", 1050));
-
     shared_ptr<Player> player1 = std::make_shared<Player>(Player("Flo", 100, "Hainkinhos"));
     shared_ptr<Player> player5 = std::make_shared<Player>(Player("Leo", 100, "Hainkinhos"));
     shared_ptr<Player> player2 = std::make_shared<Player>(Player("Simon", 100, "Bks"));
@@ -35,29 +30,7 @@ int main() {
     player1->get_hosted_game()->play(1);
     player1->get_hosted_game()->play(0);
 
-
-    cout << "Close Game" << endl;
-    player1->close_game();
-    cout << player1->get_hosted_game() << endl;
-    cout << "Count: "<< player1.use_count() << endl;
-    cout << "Game Closed" << endl;
-
     cout << *player1->get_hosted_game() << endl;
-
-    player1->get_hosted_game()->printHallOfFame();
-    auto kv = player1->get_hosted_game()->best_team();
-    cout << "The Team \"" << kv.first << "\" wins the game with " << kv.second << " wins!" << endl;
-
-    auto [bestPlayer, wins] = player1->get_hosted_game()->best_player();
-    cout << "Best Player: " << bestPlayer.get_name() << " mit " << wins << " wins!" << endl;
-
-
-
-    // cout << (*player1 == *player2 ? "True" : "False") << endl;
-
-    // cout << *player1 << endl;
-
-
 
     return 0;
 }
